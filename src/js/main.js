5a3c6553602e293dfe9eb2091de242726a911694
Taylin Large <taylin.large@gmail.com>
2017-05-14 12:49:56 -0400

    hero dating app

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+var HeroApi = (function(){
+	var shared = {};
+
+	function populate(data){
+		var results = JSON.parse(data);
+		console.log(results);
+	}
+
+	function setupListeners(){
+		$('.button-1').on('click', function(e){
+			e.preventDefault();
+			console.log('clicked');
+			$.ajax('https://gateway.marvel.com:443/v1/public/characters?apikey=7e4c5c423179dd65da3bd09829eeee78')
+			.done(populate);
+		})
+	}
+
+	function init(){
+		setupListeners();
+	}
+	shared.init = init;
+
+	return shared;
+}());
+
+HeroApi.init();
